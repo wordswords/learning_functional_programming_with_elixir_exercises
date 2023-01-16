6c8e8b9dff6134b34cdc9725196ff9859a7588f0
David Craddock <contact@davidcraddock.net>
2023-01-16 19:27:52 +0000

    Fully working TTT winner exercise

--- chp3exercises/lib/TicTacToeIsWinner.ex
+++ chp3exercises/lib/TicTacToeIsWinner.ex
@@ -1,46 +1,74 @@
 defmodule TicTacToeIsWinner do
-    def check_seq(sequence) do sequence == 3 end
 
-    def count_seq(seq, side) do
-        Enum.reduce(seq, 0, fn {v}, acc -> acc + boolean_to_integer(v == side) end)
+    defp mark_seq(side, sequence) when side == :x do 
+            if sequence == 3 do
+                throw(:x_winner)
+            else
+                :no_return
+            end
     end
 
-    def process_seq(board, side, line) when line == 'row' do
-        Enum.each(Enum.split(board, 3), fn row ->
-            check_seq(count_seq(row, side))
-        end)
+    defp mark_seq(side, sequence) when side == :o do 
+            if sequence == 3 do
+                throw(:o_winner)
+            else
+                :no_return
+            end
     end
 
-    def process_seq(board, side, line) when line == 'col' do
-        all_cols = Enum.reduce(board, [], fn({value}, acc) -> [value] ++ acc end)
-
-        Enum.each((all_cols), fn col ->
-            check_seq(count_seq(col, side))
-        end)
+    defp count_seq(seq, side) do
+        Enum.reduce(seq, 0, fn x, acc -> if x == side do acc + 1 else acc end end)
     end
 
-    def process_seq(board, side, line) when line == 'dig' do
-
-        digs = [ [board[0], board[4], board[8]], 
-                 [board[2], board[4], board[6]] ]
-
-        Enum.each(digs, fn dig ->
-                   check_seq(count_seq(dig, side))
+    defp process_seq(board, side, line) when line == 'row' do
+        all_rows = Enum.chunk_every(board, 3)
+        _ = Enum.each(all_rows, fn row -> 
+            mark_seq(side, (count_seq(row, side)))
         end)
+        :no_return
     end
 
-    def check_side_winner(board, side) do
-        Enum.each(['row','col','dig'], fn line ->
-            process_seq(board, side, line)
+    defp process_seq(board, side, line) when line == 'col' do
+        all_rows = Enum.chunk_every(board, 3)
+        all_cols = Enum.zip_with(all_rows, & &1)
+        _ = Enum.each(all_cols, fn col -> 
+            _ = mark_seq(side, count_seq(col, side))
         end)
+        :no_return
+    end
+
+    defp process_seq(board, side, line) when line == 'dig' do
+
+        all_rows = Enum.chunk_every(board, 3)
+        diagtopdown = for {row, idx} <- Enum.with_index(all_rows),
+                {col, ^idx} <- Enum.with_index(row), do: col 
+        diagdowntop = for {row, idx} <- Enum.with_index(all_rows),
+                {col, ^idx} <- Enum.with_index(Enum.reverse(row)), do: col 
+        digs = [diagtopdown, diagdowntop]
+
+        _ = Enum.each(digs, fn dig ->
+            _ = mark_seq(side, count_seq(dig, side))
+        end)
+        :no_return
+    end
+
+    defp check_side_winner(board, side) do
+        _ = Enum.each(['row','col','dig'], fn line ->
+            _ = process_seq(board, side, line)
+        end)
+        :no_return
     end
 
     def check_winner(board) do
-        check_side_winner(board, ':x') 
-        check_side_winner(board, ':o')
+        try do
+            _ = check_side_winner(board, :x)
+            _ = check_side_winner(board, :o)
+            :no_winner
+        catch 
+            :x_winner -> :x
+            :o_winner -> :o
+        end
     end
 
-    defp boolean_to_integer(true), do: 1
-    defp boolean_to_integer(false), do: 0
 
 end
--- chp3exercises/test/TicTacToeIsWinner_test.exs
+++ chp3exercises/test/TicTacToeIsWinner_test.exs
@@ -3,9 +3,27 @@ defmodule TicTacToeIsWinnerTest do
     use ExUnit.Case
 
     test "Top row x win" do
-        board = [":x",":x",":x",
-                  ":o",":o",":x",
-                  ":x",":x",":o"]
-        IO.Inspect TicTacToeIsWinner.check_winner(board)
+        board = [:x,:x,:x,
+                  :o,:o,:x,
+                  :x,:x,:o ]
+        assert TicTacToeIsWinner.check_winner(board) == :x
+    end
+    test "Top row o win" do
+        board = [:o,:o,:o,
+                  :o,:o,:x,
+                  :x,:x,:o ]
+        assert TicTacToeIsWinner.check_winner(board) == :o
+    end
+    test "Diag o win" do
+        board = [:o,:x,:x,
+                  :o,:o,:x,
+                  :x,:x,:o ]
+        assert TicTacToeIsWinner.check_winner(board) == :o
+    end
+    test "Draw" do
+        board = [:o,:x,:o,
+                  :o,:x,:x,
+                  :x,:o,:o ]
+        assert TicTacToeIsWinner.check_winner(board) == :no_winner
     end
 end
